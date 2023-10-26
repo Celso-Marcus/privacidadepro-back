1712708de2fc43c037b4519e6eb41342ef5bd9b7
Celso Brito <celso.botelho14@gmail.com>
2023-10-25 21:47:58 -0300

    initial commit

---
+++ src/main/java/com/pro/privacidade/PrivacidadeApplication.java
@@ -0,0 +1,13 @@
+package com.pro.privacidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivacidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivacidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro/privacidade/PrivacidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.privacidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivacidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
